24b93ea6969aa2de62cb503af82860b3c2f9e300
natlai123 <natlai123@users.noreply.github.com>
2017-08-31 14:59:22 -0400

    Add files via upload

---
+++ hw8/day_counter.m
@@ -0,0 +1,69 @@
+function out = day_counter(in)
+
+% Create a vector which is effectively a lookup table of the number of days
+% in each month. Then change the entry for February if the target year is a leap year.
+
+% Note that a leap year occurs on any year evenly divisible by 4, but not 
+% on a century unless it is divisible by 400. Taking that into account:
+
+if in >= 1776 && in <= 1800
+    n_day_yr = (in - 1776)*365 + ceil((in-1776)/4);  
+elseif in > 1800 && in <= 1900
+    n_day_yr = (in - 1776)*365 + ceil((in-1776)/4) - 1;  
+else
+    n_day_yr = (in - 1776)*365 + ceil((in-1776)/4) - 2;
+end
+            
+% See how many days passed between January 1 1776 and the given year,
+% adjusted for 1800 and 1900
+
+if mod(in,4) == 0  % if leap year
+   i_mon = mod(n_day_yr,7) + 1;     % See what day January 1 was that input year  
+   n_day_in = 1:366;
+   n_week_in = ceil(length(n_day_in)/7);
+   cal_mat = zeros(n_week_in*7,1);
+   cal_mat(i_mon)=1;     % Jan
+   cal_mat(i_mon+31)=1;
+   cal_mat(i_mon+31+29)=1;   % Mar
+   cal_mat(i_mon+31+29+31)=1;
+   cal_mat(i_mon+31+29+31+30)=1;    % May
+   cal_mat(i_mon+31+29+31+30+31)=1;
+   cal_mat(i_mon+31+29+31+30+31+30)=1;   % July
+   cal_mat(i_mon+31+29+31+30+31+30+31)=1; 
+   cal_mat(i_mon+31+29+31+30+31+30+31+31)=1;  % Sem
+   cal_mat(i_mon+31+29+31+30+31+30+31+31+30)=1; 
+   cal_mat(i_mon+31+29+31+30+31+30+31+31+30+31)=1; % Nov
+   cal_mat(i_mon+31+29+31+30+31+30+31+31+30+31+30)=1; 
+   cal_mat = transpose(reshape(cal_mat,7,n_week_in));
+   out=sum(cal_mat(:,1));
+   if in == 2000
+       return
+   end
+end
+
+if mod(in,4) ~= 0 || in == 1800 || in == 1900   
+   % if common year or Century year except 2000
+   i_mon = mod(n_day_yr,7) + 1;
+   n_day_in = 1:365;
+   n_week_in = ceil(length(n_day_in)/7);
+   cal_mat = zeros(n_week_in*7,1);
+   cal_mat(i_mon)=1;     % Jan 
+   cal_mat(i_mon+31)=1;
+   cal_mat(i_mon+31+28)=1;  % Mar
+   cal_mat(i_mon+31+28+31)=1;
+   cal_mat(i_mon+31+28+31+30)=1;    % May
+   cal_mat(i_mon+31+28+31+30+31)=1;
+   cal_mat(i_mon+31+28+31+30+31+30)=1;  % July
+   cal_mat(i_mon+31+28+31+30+31+30+31)=1;
+   cal_mat(i_mon+31+28+31+30+31+30+31+31)=1;    % Sem
+   cal_mat(i_mon+31+28+31+30+31+30+31+31+30)=1;
+   cal_mat(i_mon+31+28+31+30+31+30+31+31+30+31)=1;  % Nov
+   cal_mat(i_mon+31+28+31+30+31+30+31+31+30+31+30)=1;
+   cal_mat = transpose(reshape(cal_mat,7,n_week_in));
+   out=sum(cal_mat(:,1));
+end
+
+end
+
+
+
